463720b37903950ea295b33f8f0941b51cd0eaa9
Miroslav <blcham@gmail.com>
2015-11-01 00:47:31 +0100

    [FIX] engine values for cli help message

--- sdlnot-rules.sh
+++ sdlnot-rules.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
 	echo '	-m FILE -- Mapping file for resolving physical URIs of the input ontology and its imports'
 
 	echo '	-d DIR -- Directory with rules that should contain the file "rulespec" with the definition of rules in the following format: <STATE> <RULE_NAME> {<INPUT_ONTOLOGY_IRI> {<OUTPUT_ONTOLOGY_IRI>}}' 
-	echo '	-e REGIME -- Entailment regime to be used - valid values are {MIXED|PELLET|ARQ}'
+	echo '	-e REGIME -- Entailment regime to be used - valid values are {ARQ|PelletARQ|PelletMIXED|PelletONLY}'
 	echo '	-v -- Treat all variables as distinguished ones. This option makes the query result incomplete, but is significantly faster.'
 	exit	
 fi
